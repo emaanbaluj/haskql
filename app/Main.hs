8bcf9d213936e8dc65ba4af3b4b7f66512daa71f
Daniel Craciun <danielcraciun111@yahoo.com>
2025-04-08 21:00:19 +0100

    moved statement printer to lib file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
-import CQLParser (Stmt (..), parseCql)
+import CQLParser (parseCql)
 import CQLexer (alexScanTokens)
 import System.Environment (getArgs)
+import Util (statementPrinter)
 
 main :: IO ()
 main = do
@@ -10,14 +11,3 @@ main = do
   let parsed = parseCql tokens
 
   mapM_ statementPrinter parsed
-
-statementPrinter :: Stmt -> IO ()
-statementPrinter stmt = case stmt of
-  Import file var -> do
-    putStrLn $ "Import " ++ file ++ " as " ++ var
-  Print var sort trim -> do
-    putStrLn $ "Print " ++ var ++ " " ++ show sort ++ " " ++ show trim
-  Write var file -> do
-    putStrLn $ "Write " ++ var ++ " to " ++ file
-  Set var query -> do
-    putStrLn $ "Set " ++ var ++ " as " ++ show query
--- src/Util.hs
+++ src/Util.hs
@@ -1,7 +1,20 @@
-module Util (splitOn) where
+module Util (splitOn, statementPrinter) where
+
+import CQLParser (Stmt (..))
 
 splitOn :: Char -> String -> [String]
-splitOn delimitter input =
-  case break (== delimitter) input of
-    (x, _ : xs) -> x : splitOn delimitter xs
+splitOn delimiter input =
+  case break (== delimiter) input of
+    (x, _ : xs) -> x : splitOn delimiter xs
     (x, _) -> [x]
+
+statementPrinter :: Stmt -> IO ()
+statementPrinter stmt = case stmt of
+  Import file var -> do
+    putStrLn $ "Import " ++ file ++ " as " ++ var
+  Print var sort trim -> do
+    putStrLn $ "Print " ++ var ++ " " ++ show sort ++ " " ++ show trim
+  Write var file -> do
+    putStrLn $ "Write " ++ var ++ " to " ++ file
+  Set var query -> do
+    putStrLn $ "Set " ++ var ++ " as " ++ show query
