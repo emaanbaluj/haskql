31d59dfa12a27c3b3014f28a1474a1f578ab96f1
Daniel Craciun <danielcraciun111@yahoo.com>
2025-04-08 20:57:20 +0100

    created base interpreter structure to print the parsed statements

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-import CQLParser (parseCql)
+import CQLParser (Stmt (..), parseCql)
 import CQLexer (alexScanTokens)
 import System.Environment (getArgs)
 
@@ -8,4 +8,16 @@ main = do
   contents <- readFile filename
   let tokens = alexScanTokens contents
   let parsed = parseCql tokens
-  print parsed
+
+  mapM_ statementPrinter parsed
+
+statementPrinter :: Stmt -> IO ()
+statementPrinter stmt = case stmt of
+  Import file var -> do
+    putStrLn $ "Import " ++ file ++ " as " ++ var
+  Print var sort trim -> do
+    putStrLn $ "Print " ++ var ++ " " ++ show sort ++ " " ++ show trim
+  Write var file -> do
+    putStrLn $ "Write " ++ var ++ " to " ++ file
+  Set var query -> do
+    putStrLn $ "Set " ++ var ++ " as " ++ show query
