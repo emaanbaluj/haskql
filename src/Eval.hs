15b2341167910c61826e152ae41f27184bab3be1
Daniel Craciun <danielcraciun111@yahoo.com>
2025-04-10 23:01:44 +0100

    cartesian product example in interpreter

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,8 +1,9 @@
 module Eval (eval) where
 
-import CQLParser (SortOrder (ASC, DESC), Stmt (..), Trim (TrimFalse, TrimTrue))
+import CQLParser (Query (Cross), SortOrder (ASC, DESC), Stmt (..), Trim (TrimFalse, TrimTrue))
 import Control.Monad.State
 import qualified Data.Map as Map
+import Data.Maybe (fromJust)
 import State (addToContext)
 import Types (CSVData, CSVState)
 import Util (convertToCSV, readCSV, trimString)
@@ -15,6 +16,15 @@ eval (Print var sort trim) = do
   ctx <- get
   let result = Map.lookup var ctx
   liftIO $ putStr $ formatResult result sort trim
+eval (Set var query) = do
+  ctx <- get
+  case query of
+    Cross [var1, var2] -> do
+      let result1 = Map.lookup var1 ctx
+      let result2 = Map.lookup var2 ctx
+      let result = [row1 ++ row2 | row1 <- fromJust result1, row2 <- fromJust result2]
+      addToContext var result
+    _ -> return ()
 eval _ = return ()
 
 formatResult :: Maybe CSVData -> SortOrder -> Trim -> String
